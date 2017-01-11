1e2a87c8b188bb3202afd62d55092f6bbb63838f
gaeun917 <stila917@gmail.com>
2017-01-11 09:40:12 +0900

    changed indentation and comment section to make sure the intention of the code is clear

--- src/com/team5/servlet/BoardDetailServlet.java
+++ src/com/team5/servlet/BoardDetailServlet.java
@@ -20,57 +20,47 @@ import com.team5.dto.Upload;
 
 @WebServlet(value = "/board/detail.action")
 public class BoardDetailServlet extends HttpServlet {
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-						throws ServletException, IOException {
-		
-		//1. 요청 데이터 읽기
-		String BoardNo = req.getParameter("boardno");
-		if(BoardNo == null || BoardNo.length() == 0) {
-			resp.sendRedirect("/demoweb/board/list.action");//읽지 못하면 리스트로 이동
 
-			return;
-		}
-		//문자열을 숫자로 변경
-		int iBoardNo = Integer.parseInt(BoardNo);	
-		//BoardListServlet에서 페이지 번호 읽기 코드 copy&paste
-	    // 요청된 페이지 번호 읽기 (없으면 1로 설정)
-					String pageNo = req.getParameter("pageno");
-					int currentPage = 1; //현재 페이지 번호 
-					if(pageNo != null && pageNo.length()>0){
-						currentPage = Integer.parseInt(pageNo);
-					}
-					
-		//현재 페이지 번호를 request에 저장 -> jsp 사용 
-					req.setAttribute("pageno", currentPage);
-				
-		//2. 자료 번호로 자료 정보 조회 (DAO)
-		BoardDao dao = new BoardDao();
-		Board b = dao.selectBoardByBoardNo(iBoardNo);
-		
-		//조회수 증가-> 화면 리프레쉬하면 조회수 증가 
-		// jsp가 안나오기때문에 다운로드가 됨. 
-//		dao.updateBoardReadCount(iBoardNo);
-//		b.setReadCount(b.getReadCount()+1);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-		dao.updateBoardread_cnt(iBoardNo);
-		b.setReadCnt(b.getReadCnt()+1);
-		/*dao.updateBoardReadCount(iBoardNo);
-		b.setReadCount(b.getReadCount()+1);*/
-		//3. 조회된 자료를 jsp에서 읽을 수 있도록 req에 저장
-		
-		req.setAttribute("board", b);
-		
-		//4. 조회된 정보 표시
-		RequestDispatcher dispatcher = 
-			req.getRequestDispatcher(
-					"/WEB-INF/views/board/detail.jsp");
-		dispatcher.forward(req, resp);
+        //1. 요청 데이터 읽기
+        String BoardNo = req.getParameter("boardno");
+        String pageNo = req.getParameter("pageno");
 
-	}
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
-	}
+            //1.1 보드 넘버 구하기
+            if (BoardNo == null || BoardNo.length() == 0) {
+                resp.sendRedirect("/demoweb/board/list.action");//읽지 못하면 리스트로 이동
+                return;
+            }
+            int iBoardNo = Integer.parseInt(BoardNo); //String to Integer
+            //1.2 페이지 넘버 구하기
+            int currentPage = 1; //현재 페이지 번호
+            if (pageNo != null && pageNo.length() > 0) {
+                currentPage = Integer.parseInt(pageNo);
+            }
+
+        //2. 현재 페이지#를 request에 저장(jsp에서 사용)
+        req.setAttribute("pageno", currentPage);
+
+        BoardDao dao = new BoardDao();
+        Board b = dao.selectBoardByBoardNo(iBoardNo);//DAO 접근해서 해당 보드 가져오기
+
+        //3. 조회수 카운트 (화면 리프레쉬하면 조회수 증가)
+        dao.updateBoardread_cnt(iBoardNo);
+        b.setReadCnt(b.getReadCnt() + 1);
+
+        req.setAttribute("board", b);// req에 저장
+
+        //4. 조회된 정보 표시
+        RequestDispatcher dispatcher =
+                req.getRequestDispatcher("/WEB-INF/views/board/detail.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 }
--- src/com/team5/servlet/BoardEditFormServlet.java
+++ src/com/team5/servlet/BoardEditFormServlet.java
@@ -1,10 +1,6 @@
 package com.team5.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,44 +14,33 @@ import com.team5.dto.Board;
 @WebServlet("/board/edit.action")
 public class BoardEditFormServlet extends HttpServlet {
 
-	// 2. 1의 글 번호로 데이터 조회(없으면 목록으로)
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		// 1. 요청 데이터 읽기 (상세 정보를 표시할 자료 번호)
-		// 읽지 못하면 리스트로 이동
 		String BoardNo = req.getParameter("boardno");
-		if (BoardNo == null || BoardNo.length() == 0) {
-			resp.sendRedirect("/demoweb/board/list.action");
-			return;
-		}
-		
-		// 문자열을 숫자로 변경
-		int iBoardNo = Integer.parseInt(BoardNo);
-		// BoardListServlet에서 페이지 번호 읽기 코드 copy&paste
-		// 요청된 페이지 번호 읽기 (없으면 1로 설정
+			if (BoardNo == null || BoardNo.length() == 0) {
+				resp.sendRedirect("list.action");// 읽지 못하면 리스트로 이동
+				return;
+			}
+			int iBoardNo = Integer.parseInt(BoardNo);//String to Interger
 
-		// 2. 자료 번호로 자료 정보 조회 (DAO)
+
+		//2. BoardNmber를 request에 저장
 		BoardDao dao = new BoardDao();
-		Board b = dao.selectBoardByBoardNo(iBoardNo);
-
-		if (BoardNo == null || BoardNo.length() == 0) {
-			resp.sendRedirect("list.action");
-			return;
-		}
-		// 현재 페이지 번호를 request에 저장 -> jsp 사용
+		Board b = dao.selectBoardByBoardNo(iBoardNo);//DAO 접근 BoardNo로 해당보드 저장
 		req.setAttribute("board", b);
 
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/board/editform.jsp");
-		dispatcher.forward(req, resp);
 
+		//3. redirect
+		RequestDispatcher dispatcher =
+				req.getRequestDispatcher("/WEB-INF/views/board/editform.jsp");
+		dispatcher.forward(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		req.setCharacterEncoding("utf-8");
-
+		req.setCharacterEncoding("utf-8");// filter에서 해도되지 않나?
 		doGet(req, resp);
 	}
 
